80eb662f355d9b0156076f864b2183af12abc990
ELELOUE Lynda <rsma.simplon21@gmail.com>
2021-05-18 10:13:26 -0400

    evaluation stage le 18 mai 2021

---
+++ rsma-lynda.gp/traitement.php
@@ -0,0 +1,26 @@
+<?php 
+ini_set('display_errors', 1);
+
+// autoloader
+spl_autoload_register(function ($name) {
+    echo "Tentative de chargement de $name.\n";
+    throw new Exception("Impossible de charger $name.");
+});
+
+try {
+   // instanciation des classes
+   include("classes/utilisateur.php");
+   
+
+} catch (Exception $e) {
+    echo $e->getMessage(), "\n";
+}
+
+
+    // traitement formulaire rdv
+    $user = new utilisateur();
+    $user->formulaire($_POST);
+
+    print_r($user);
+
+
